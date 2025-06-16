fbc58b901b3a8d35d45481a8dfea2ec6fdf41814
victor_samurai <victor.silva@samuraitratores.com.br>
2025-06-16 17:16:05 -0300

    criacao de projeto QR-Code

---
+++ script.js
@@ -0,0 +1,22 @@
+const formulario = document.getElementById('meuFormulario');
+          
+formulario.addEventListener('submit', function(event) {
+  event.preventDefault(); // Impede o envio padrão do formulário
+
+  const formData = new FormData(formulario);
+  const dadosFormulario = {};
+
+  for (const [chave, valor] of formData.entries()) {
+    dadosFormulario[chave] = valor;
+  }
+
+  console.log("Dados do formulário:", dadosFormulario);
+
+  const nome = formData.get('nome');
+  console.log("Nome:", nome);
+
+  if (nome == 'S@M20442ti') {
+    window.location.href = "file:///C:/Users/victo/OneDrive/%C3%81rea%20de%20Trabalho/qr_code_charleson.html"
+  }
+
+});
